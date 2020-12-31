e056258bb8796b7f45ec601c1d135ea30a37e55c
sml0399 <sml0399@naver.com>
2020-12-31 22:55:57 +0900

    modified data_loader.py

--- CF_recommender_system/Netflix/data_loader.py
+++ CF_recommender_system/Netflix/data_loader.py
@@ -4,19 +4,72 @@ import numpy as np
 # user id/ item id /rating /timestamp
 
 
-def loader_100k():
-	f = open(os.path.dirname(__file__) + '../../dataset/MovieLens/1M_dataset/ratings.dat',"r")
+def loader_1M():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/1M_dataset/ratings.dat',"r")
 	text=f.read().split('\n')
 	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("::")[0:3]]) for row in text[0:-1]])
-	print(new_text[0:2])
+	return new_text
 
 
-def loader_1M():
-	f = open(os.path.dirname(__file__) + '../../dataset/MovieLens/100k_dataset/u1.base',"r")
+# train_set
+def loader_100k_1():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u1.base',"r")
 	text=f.read().split('\n')
 	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
-	print(new_text[0:2])
+	return new_text
+
+def loader_100k_2():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u2.base',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_3():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u3.base',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_4():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u4.base',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_5():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u5.base',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
 
 
-loader_100k()
-loader_1M()
+# test_set
+def loader_100k_t1(): 
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u1.test',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_t2():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u2.test',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_t3():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u3.test',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_t4():
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u4.test',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
+
+def loader_100k_t5(): 
+	f = open(os.path.dirname(__file__) + '/../../dataset/MovieLens/100k_dataset/u5.test',"r")
+	text=f.read().split('\n')
+	new_text=np.asarray([np.asarray([np.float(element) for element in row.split("\t")[0:3]]) for row in text[0:-1]])
+	return new_text
---
+++ CF_recommender_system/Netflix/test.py
@@ -0,0 +1,2 @@
+import data_loader as dl
+print(dl.loader_1M_1()[0])
